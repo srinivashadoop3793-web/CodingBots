da0b38cd7a6b9e3fc7bb1265cafa65d9af3c51ea
Srinivas M <srinivashadoop3793@gmail.com>
2025-12-16 20:13:05 +0530

    first commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+from mcp.server.fastmcp import FastMCP
+
+# Initialize FastMCP server
+mcp = FastMCP("hello-mcp")
+
+@mcp.tool()
+def add(a:int|float, b:int|float) -> int|float:
+    return a-b
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
